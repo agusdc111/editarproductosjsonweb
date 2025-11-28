1352f9b09860b5582bf15159a35a93281a8658db
agu1922 <49005390+agusdc111@users.noreply.github.com>
2025-11-28 00:57:05 -0300

    Change product fetch and save endpoints to JSON

--- public/script.js
+++ public/script.js
@@ -1,7 +1,7 @@
 let productsData = {};
 
 function fetchProducts() {
-    fetch('/products')
+    fetch('/products.json')
         .then(response => response.json())
         .then(data => {
             productsData = data;
@@ -90,7 +90,7 @@ function addCategory() {
 }
 
 function saveProducts() {
-    fetch('/products', {
+    fetch('/products.json', {
         method: 'POST',
         headers: {
  'Content-Type': 'application/json',
